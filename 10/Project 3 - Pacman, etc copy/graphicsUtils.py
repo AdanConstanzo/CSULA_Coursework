3a50bf6d1af7a120380d491600f5de4b00dc2763
Adan Constanzo <aconstanzolan@gmail.com>
2018-11-15 17:52:53 -0800

    Adding some stuff from homework 10.

--- 10/Project 3 - Pacman, etc copy/graphicsUtils.py
+++ 10/Project 3 - Pacman, etc copy/graphicsUtils.py
@@ -54,7 +54,7 @@ def sleep(secs):
         _root_window.after(int(1000 * secs), _root_window.quit)
         _root_window.mainloop()
 
-def begin_graphics(width=640, height=480, color=formatColor(0, 0, 0), title=None):
+def begin_graphics(width=0, height=480, color=formatColor(0, 0, 0), title=None):
 
     global _root_window, _canvas, _canvas_x, _canvas_y, _canvas_xs, _canvas_ys, _bg_color
 
